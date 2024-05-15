de5a13b6a8d3eb88ccf294b2d77561c0b757f369
nedretcelik <nedretcelik97@gmail.com>
2024-05-15 17:47:09 -0400

    update

--- exercises/dynamic-images/scripts/index.js
+++ exercises/dynamic-images/scripts/index.js
@@ -5,12 +5,12 @@
 let imageFiles = [
   {
     id: "1",
-    path: "https://placehold.co/100x300",
+    path: "https://placehold.co/300x300",
     description: "placeholder 1",
   },
   {
     id: "2",
-    path: "https://placehold.co/200x300",
+    path: "https://placehold.co/300x300",
     description: "placeholder 2",
   },
   {
@@ -20,49 +20,51 @@ let imageFiles = [
   },
   {
     id: "4",
-    path: "https://placehold.co/400x300",
+    path: "https://placehold.co/300x300",
     description: "placeholder 4",
   },
   {
     id: "5",
-    path: "https://placehold.co/500x300",
+    path: "https://placehold.co/300x300",
     description: "placeholder 5",
   },
   {
     id: "6",
-    path: "https://placehold.co/600x300",
+    path: "https://placehold.co/300x300",
     description: "placeholder 6",
   },
   {
     id: "7",
-    path: "https://placehold.co/700x300",
+    path: "https://placehold.co/300x300",
     description: "placeholder 7",
   },
   {
     id: "8",
-    path: "https://placehold.co/800x300",
+    path: "https://placehold.co/300x300",
     description: "placeholder 8",
   },
   {
     id: "9",
-    path: "https://placehold.co/900x300",
+    path: "https://placehold.co/300x300",
     description: "placeholder 9",
   },
   {
     id: "10",
-    path: "https://placehold.co/950x300",
+    path: "https://placehold.co/300x300",
     description: "placeholder 10",
   },
 ];
 
-let imageList = document.getElementById("imageList");
-let imageContainer = document.getElementById("imageContainer");
-let displayImageButton = document.getElementById("displayImageBtn");
+let imageList = document.querySelector("#imageList");
+let imageContainer = document.querySelector("#imageContainer");
+let displayImageButton = document.querySelector("#displayImageBtn");
+let clearButton = document.querySelector("#clearButton");
 
 window.onload = function () {
   createImageList();
 
   displayImageButton.onclick = displayMatchingImages;
+  clearButton.onclick = clearImage;
 };
 
 function createImageList() {
@@ -70,22 +72,71 @@ function createImageList() {
     let imageOption = new Option("option");
 
     imageOption.innerText = imageFile.description;
-    imageOption.value = imageFile.description;
-
+    imageOption.value = imageFile.id;
 
     imageList.appendChild(imageOption);
   }
 }
 
+
+
 function displayMatchingImages() {
 
   for (const imageFile of imageFiles) {
-    if (imageFile.description == imageList.value) {
+    if (imageFile.id == imageList.value) {
       let image = document.createElement("img");
       image.src = imageFile.path;
-      image.alt = imageFile.description;
+      image.alt = imageFile.id;
 
       imageContainer.appendChild(image);
     }
   }
 }
+
+
+function clearImage() {
+  imageContainer.innerText = ""
+
+}
+
+
+
+
+
+/* window.onload = function () {
+  //get all html elements into variables
+  const imageFileDropdown = document.querySelector("#imageFileDropdown");
+  const addImageButton = document.querySelector("#addImageButton");
+  const clearImagesButton = document.querySelector("#clearImagesButton");
+  const imageContainerDiv = document.querySelector("#imageContainerDiv");
+
+  //define  your functions
+
+  function loadImageFileDropdown() {
+    for (const imageFile of imageFiles) {
+      let option = document.createElement("option");
+      option.value = imageFile.id;
+      option.innerText = imageFile.description;
+      imageFileDropdown.appendChild(option);
+    }
+  }
+
+  function addImage() {
+    const id = imageFileDropdown.value;
+    const imageFile = imageFiles.find((imageFile) => imageFile.id == id);
+
+    let image = document.createElement("img");
+    image.src = imageFile.path;
+    image.alt = imageFile.description;
+    imageContainerDiv.appendChild(image);
+  }
+
+  function clearImages() {
+    imageContainerDiv.innerHTML = "";
+  }
+
+  //associate your functions with html element events
+  addImageButton.onclick = addImage;
+  clearImagesButton.onclick = clearImages;
+  loadImageFileDropdown();
+}; */
